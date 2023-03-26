adc504ef02850c3e56367ec0144c4aefcd7d6c91
Quwsar Ohi <quwsarohi@gmail.com>
2023-03-26 13:57:56 -0600

    T5 model created

--- model/T5.py
+++ model/T5.py
@@ -32,7 +32,7 @@ class T5(Module):
         self.tokenizer          = T5Tokenizer.from_pretrained(variant)
         self.model              = T5ForConditionalGeneration.from_pretrained(variant)
         
-        # Optimier
+        # Optimizer
         self.optimizer          = torch.optim.AdamW(self.parameters(), **optimizer_config)
         
         # Scheduler
@@ -42,20 +42,30 @@ class T5(Module):
     def tokenize(self, input:List[str]):
 
         out = self.tokenizer(input, max_length=self.max_source_length,
-                             truncation=True, padding=True, 
+                             truncation=False, padding=True, 
                              return_tensors="pt")
 
         return out.input_ids, out.attention_mask
 
 
-    def forward(self, input:List[str], target:Optional[List[str]]=None) -> Tuple[Tensor, Optional[Tensor]]:
+    def forward(self, input:List[str], label:Optional[List[str]]=None) -> Tuple[Tensor, Optional[Tensor]]:
 
         '''
         Will receive input and target string and produce the final output as tensor (not decoded)
         when target is not None, it will give the loss functions with the output as tuple
         '''
+        
+        input_ids, input_masks = self.tokenize(input)
+        
+        if label is not None:
+            label_ids, label_masks = self.tokenize(label)    
+            output = self.model(input_ids=input_ids, labels=label_ids)
+            return output.logits, output.loss
+            
+        return self.model.generate(input_ids=input_ids,
+                                   max_length=128,
+                                   max_new_tokens=20), None
 
-        pass
 
 
     def predict(self, input:List[str]) -> List[str]:
@@ -63,8 +73,11 @@ class T5(Module):
         '''
         Will generate the target output as string
         '''
-
-        pass
+        
+        logits, loss = self.forward(input=input)
+        
+        return self.tokenizer.batch_decode(logits, skip_special_tokens=True)
+        
 
 
 if __name__ == '__main__':
@@ -72,4 +85,31 @@ if __name__ == '__main__':
     '''
     Implement a tester class similar to T5-old.py to test if it works
     '''
+    
+    model = T5('t5-small')
+
+    inputs = [
+        "translate English to German: Thank you so much, Chris.",
+        "translate English to German: I have been blown away by this conference, and I want to thank all of you for the many nice comments about what I had to say the other night.",
+        "translate German to English: Es ist mir wirklich eine Ehre, zweimal auf dieser Bühne stehen zu dürfen. Tausend Dank dafür.",
+    ]
+
+    targets = [
+        "Vielen Dank, Chris.",
+        "Ich bin wirklich begeistert von dieser Konferenz, und ich danke Ihnen allen für die vielen netten Kommentare zu meiner Rede vorgestern Abend.",
+        "And it's truly a great honor to have the opportunity to come to this stage twice; I'm extremely grateful.",
+    ]
+
+    logits, loss = model.forward(inputs, targets)
+    print('Model forward')
+    print('logits: ', logits)
+    print('loss: ', loss)
+    
+    outputs = model.predict(inputs)
+    
+    print('OUTPUT')
+    print(outputs)
+    #for (inp, out), tar in zip(zip(inputs, outputs), targets):
+    #    print(f"Input: {inp}\nOutput: {out}\nTarget: {tar}\n")
+
 
